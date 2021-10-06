995800a5cf173914f0f675a26061e75825be0ad6
FiF0o <jonathan.lazarini@wanadoo.fr>
2021-10-06 18:41:05 +0100

    create post request and event added

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,12 +1,21 @@
+import * as Constants from '../../constant'
+
 async function createAuction(event, context) {
   console.log({event})
   console.log({context})
+
+  const {title} = JSON.parse(event.body)
+  const auction = {
+    title,
+    status: Constants.AUCTION_STATUS.OPEN,
+    createdAt: new Date().toISOString()
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello' }),
+    statusCode: 201,
+    body: JSON.stringify(auction),
   };
 }
 
 export const handler = createAuction;
-
 
